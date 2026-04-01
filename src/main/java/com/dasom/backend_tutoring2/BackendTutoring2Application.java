d4065fbe0db6edac6153720803e1dde36b61d37d
cgh0-0 <marketcarrot280@gmail.com>
2026-04-01 18:29:59 +0900

    숙제 수행중

---
+++ src/main/java/com/dasom/backend_tutoring2/BackendTutoring2Application.java
@@ -0,0 +1,13 @@
+package com.dasom.backend_tutoring2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTutoring2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendTutoring2Application.class, args);
+	}
+
+}
